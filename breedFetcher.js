fe7abdb69545a1d9e024564746903dfefe0e69dc
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-03-21 19:53:58 +0000

    refactored breedFetcher.js, created index.js and breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,27 @@
 
 const request = require('request');
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-
-if (!breedName) {
-  console.error('You must provide a valid breed name.');
-  process.exit(1); // Exits the program indicating that an error occurred
-}
-
-
-request(url, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   
-  if (error !== undefined) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
-    const data = JSON.parse(body);
- 
-    if (data.length === 0) {
-      console.log("breed not found");
-    } else {
-      console.log((data[0].description));
+  // if error from url
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
     }
-  }
+    
+    const data = JSON.parse(body);
+    const breed = data[0];
+    
+    // if breed is valid, if it isnt
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback("Breed not found");
+    }
+    
   
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
